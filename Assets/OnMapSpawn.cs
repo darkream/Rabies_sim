854fdaa6bb0d5f015d1041810930bfbb54f4e89b
alternativejinnrakung <alternativejinnrakung@gmail.com>
2018-11-12 17:02:37 +0700

    add world height in meters
    Ground height level is calculated by color coding on the map. Each dog will keep the ground height (meters). Moreover, the moving [lat, lon] in meters now covers the function of Mapbox, which will make the map having Mapbox's map accuracy.

--- Assets/OnMapSpawn.cs
+++ Assets/OnMapSpawn.cs
@@ -3,8 +3,8 @@ using Mapbox.Utils;
 using Mapbox.Unity.Map;
 using Mapbox.Unity.Utilities;
 using System.Collections.Generic;
-using Mapbox.Unity.MeshGeneration.Factories;
-using Mapbox.Geocoding;
+using Mapbox.Map;
+using Mapbox.Unity.MeshGeneration.Data;
 
 public class OnMapSpawn : MonoBehaviour
 {
@@ -26,13 +26,24 @@ public class OnMapSpawn : MonoBehaviour
     GameObject dogpanel; //Prefabs for dog layer
 
     [SerializeField]
-    float radius_earth = 6378.1f; // Radius of the earth in km
+    float radius_earth = 6378.1f; // Radius of the earth in km (Google Answer)
 
-    List<float> doglat, doglon;
+    [SerializeField] //(reference: http://www.longitudestore.com/how-big-is-one-gps-degree.html)
+    float equator_latitude_per_degree = 110570.0f; //110 km per degree
+    float pole_latitude_per_degree = 111690.0f; //111.69 km per degree
+    float widest_longitude_per_degree = 111321.0f; //111.321 km longitude per degree at equator (while 0 at pole)
+    float one_degree_per_radian = 0.0174532925f; //PI divided by 180
+
+
+    //Data List of Arrays
+    List<float> doglat, doglon, doggroundheight;
     List<GameObject> dogObjs;
 
     [SerializeField]
     public Camera _referenceCamera;
+
+    [SerializeField]
+    public GameObject DogLayer;
 
     void Start()
     {
@@ -40,57 +51,17 @@ public class OnMapSpawn : MonoBehaviour
         doglocations = new List<Vector2d>(); //initialization for the dog object
         doglat = new List<float>(); //initialization for lat, long
         doglon = new List<float>();
+        doggroundheight = new List<float>();
         dogObjs = new List<GameObject>();
         for (int i = 0; i < initSize; i++)
         {
             addDogLocation(doglocationStrings[i]);
         }
-        Debug.Log(getDistanceFromLatLonInKm(doglat[0] , doglon[0] , doglat[1] , doglon[1]) + " km");
-        float newlon = doglon[0] + addLonByMeters(5000.0f); // move upward by 5 km
-        float newlat = doglat[0] + addLatByMeters(5000.0f); // move right by 5 km
-        Debug.Log("move up from " + doglon[0] + " to " + newlon);
-        Debug.Log("move right from " + doglat[0] + " to " + newlat);
-        addDogLocation(newlat , newlon);
     }
 
     private void Update()
     {
-        //---------------------------------------------------------------------------------------
-        //dog distribute zone
-        //distribute tester
-        //update time
-        if (Input.GetKeyDown("z"))
-        {
-           // distribute dog top side on every existed dog 
-           int tempcount=dogObjs.Count; //prevent unlimited loophole
-            for (int j = 0; j < tempcount; j++)
-            {
-
-                // distribute dog top side on every existed dog 
-                float templon = doglon[j] ;
-                float templat = doglat[j] + addLatByMeters(1000.0f); 
-                //check that new lat lon is same as old one
-               Distribute_add(templat,templon,tempcount);
-               // distribute dog down side on every existed dog 
-                 templon = doglon[j] ;
-                templat = doglat[j] + addLatByMeters(-1000.0f); 
-                //check that new lat lon is same as old one
-               Distribute_add(templat,templon,tempcount);
-               // distribute dog rigth side on every existed dog 
-                templon = doglon[j] + addLonByMeters(1000.0f); 
-                templat = doglat[j] ; 
-                //check that new lat lon is same as old one
-               Distribute_add(templat,templon,tempcount);
-               // distribute dog left side on every existed dog 
-                templon = doglon[j] + addLonByMeters(-1000.0f); 
-                templat = doglat[j] ; 
-                //check that new lat lon is same as old one
-               Distribute_add(templat,templon,tempcount);
-            }
-        }
-        //---------------------------------------------------------------------------------------   
-
-
+        zdistribution();
 
         for (int i = 0; i < dogObjs.Count; i++) //for each spawn object
         {
@@ -100,56 +71,29 @@ public class OnMapSpawn : MonoBehaviour
             spawnedObject.transform.localScale = new Vector3(_spawnScale , _spawnScale , _spawnScale);
         }
 
-
         //on click spawn dog
         if (Input.GetMouseButtonUp(1))
         {
-            var mousePosScreen = Input.mousePosition;
+            Vector3 mousePosScreen = Input.mousePosition;
             //assign distance of camera to ground plane to z, otherwise ScreenToWorldPoint() will always return the position of the camera
             //(reference: http://answers.unity3d.com/answers/599100/view.html)
             mousePosScreen.z = _referenceCamera.transform.localPosition.y;
-            var pos = _referenceCamera.ScreenToWorldPoint(mousePosScreen);
+            Vector3 pos = _referenceCamera.ScreenToWorldPoint(mousePosScreen);
 
-            var latlongDelta = _map.WorldToGeoPosition(pos);
+            Vector2d latlongDelta = _map.WorldToGeoPosition(pos);
             addDogLocation(latlongDelta); //add new dog object from clicked position
         }
     }
 
-    //-----------------------------------------------
-    //dog distribute zone
-        private void Distribute_add(float templat,float templon,int tempcount)
-        {
-                bool check_exist=false;
-                for (int k = 0; k < tempcount; k++)
-                {
-                    if (templon==doglon[k]&&templat==doglat[k]) //if distribute pos got object
-                    {
-                    // add dog number that not exist in script yet
-                    Debug.Log("Dog distribute to other dog grid,add up");
-                    check_exist=true;
-                    }
-                }  
-                if (!check_exist) //if is new pos
-                {
-                    addDogLocation(templat , templon);
-                }
-        }
-
-
-    //----------------------------------------------
-
-
-
-
     //Harversine Formula, 
     //(reference: https://stackoverflow.com/questions/639695/how-to-convert-latitude-or-longitude-to-meters)
-    private float getDistanceFromLatLonInKm(float lat1, float lon1, float lat2, float lon2)
+    private float getDistanceFromLatLonInKm(float lat1 , float lon1 , float lat2 , float lon2)
     {
-        float dLat = deg2rad(lat2 - lat1);  // deg2rad below
-        float dLon = deg2rad(lon2 - lon1);
-        float a = 
+        float dLat = (lat2 - lat1) * one_degree_per_radian;  // deg2rad below
+        float dLon = (lon2 - lon1) * one_degree_per_radian;
+        float a =
           Mathf.Sin(dLat / 2.0f) * Mathf.Sin(dLat / 2.0f) +
-          Mathf.Cos(deg2rad(lat1)) * Mathf.Cos(deg2rad(lat2)) *
+          Mathf.Cos((lat1 * one_degree_per_radian)) * Mathf.Cos((lat2 * one_degree_per_radian)) *
           Mathf.Sin(dLon / 2.0f) * Mathf.Sin(dLon / 2.0f)
           ;
         float c = 2.0f * Mathf.Atan2(Mathf.Sqrt(a) , Mathf.Sqrt(1.0f - a));
@@ -157,15 +101,10 @@ public class OnMapSpawn : MonoBehaviour
         return d;
     }
 
-    private float deg2rad(float deg)
-    {
-        return deg * (Mathf.PI / 180.0f);
-    }
-
     //Directions of this function indicate by +/- of meter value
     private float addLatByMeters(float meter) //return the increased or decreased Lat by meter
     {
-        return meter / 111111.0f ; // because sin(90 degree) is 1
+        return meter / 111111.0f; // because sin(90 degree) is 1
     }
 
     //Directions of this function indicate by +/- of meter value
@@ -175,24 +114,40 @@ public class OnMapSpawn : MonoBehaviour
     }
 
     //Original [lat, long] add meter conversion
-    private float addLatByMeters(float meter, float theta) 
+    private float addLatByMeters(float meter , float theta)
     {
         return (meter / 111111.0f) * Mathf.Sin(theta);
     }
 
-    private float addLonByMeters(float meter, float theta)
+    private float addLonByMeters(float meter , float theta)
     {
         return (meter / 111111.0f) * Mathf.Cos(theta);
     }
 
+    //using Mapbox Conversion return length of meter in lat, lon distance
+    private float addLatByMetersMapbox(float meter)
+    {
+        return (float)Conversions.MetersToLatLon(new Vector2d(meter , 0.0)).x;
+    }
+
+    private float addLonByMetersMapbox(float meter)
+    {
+        return (float)Conversions.MetersToLatLon(new Vector2d(0.0 , meter)).y;
+    }
+
+    private Vector2d addLatLonByMetersMapbox(Vector2d latlonvector)
+    {
+        return Conversions.MetersToLatLon(latlonvector);
+    }
+
     //Add new dog location by float lat lon
-    private void addDogLocation(float lat, float lon)
+    private void addDogLocation(float lat , float lon)
     {
         doglocations.Add(Conversions.StringToLatLon(lat + ", " + lon));
         createDogObject();
     }
 
-    //Add new doglocation by location string of vector lat lon
+    //Add new doglocation by location string of vector lat lon, Formula: "lat, long"
     private void addDogLocation(string locationstring)
     {
         doglocations.Add(Conversions.StringToLatLon(locationstring));
@@ -206,15 +161,108 @@ public class OnMapSpawn : MonoBehaviour
         createDogObject();
     }
 
-    //create the object to the map location
+    //create the object to the map location (with default height)
     private void createDogObject()
     {
         int lastIndex = doglocations.Count - 1;
         doglat.Add((float)doglocations[lastIndex].x);
         doglon.Add((float)doglocations[lastIndex].y);
-        GameObject obj = Instantiate(dogpanel); //use the dog prefab to the map
-        obj.transform.localPosition = _map.GeoToWorldPosition(doglocations[lastIndex] , true);
+        spawnDogPrefabWithHeight(doglat[lastIndex] , doglon[lastIndex]);
+    }
+
+    //create the object to the map location (with calculated map height)
+    //(reference: https://github.com/mapbox/mapbox-unity-sdk/issues/222)
+    void spawnDogPrefabWithHeight(double lat , double lon)
+    {
+        //get tile ID
+        var tileIDUnwrapped = TileCover.CoordinateToTileId(new Mapbox.Utils.Vector2d(lat , lon) , (int)_map.Zoom);
+
+        //get tile
+        UnityTile tile = _map.MapVisualizer.GetUnityTileFromUnwrappedTileId(tileIDUnwrapped);
+
+        //lat lon to meters because the tiles rect is also in meters
+        Vector2d v2d = Conversions.LatLonToMeters(new Mapbox.Utils.Vector2d(lat , lon));
+        //get the origin of the tile in meters
+        Vector2d v2dcenter = tile.Rect.Center - new Mapbox.Utils.Vector2d(tile.Rect.Size.x / 2.0 , tile.Rect.Size.y / 2.0);
+        //offset between the tile origin and the lat lon point
+        Vector2d diff = v2d - v2dcenter;
+
+        //maping the diffetences to (0-1)
+        float Dx = (float)(diff.x / tile.Rect.Size.x);
+        float Dy = (float)(diff.y / tile.Rect.Size.y);
+
+        //height in unity units
+        var h = tile.QueryHeightData(Dx , Dy);
+
+        //height in meter
+        float height_in_meter = h / tile.TileScale; //*This is important, check out the function in UnityTile.cs
+        doggroundheight.Add(height_in_meter); //stored ground height to the array of that specific dog
+        Debug.Log("World Height: " + height_in_meter + " meter(s)");
+
+        //lat lon to unity units
+        Vector3 location = Conversions.GeoToWorldPosition(lat , lon , _map.CenterMercator , _map.WorldRelativeScale).ToVector3xz();
+        //replace y in position
+        location = new Vector3(location.x , h , location.z);
+
+        var obj = Instantiate(dogpanel);
+        obj.transform.position = location;
         obj.transform.localScale = new Vector3(_spawnScale , _spawnScale , _spawnScale);
+        obj.transform.parent = DogLayer.transform; //let the dog becomes the child of DogLayer game object
+
         dogObjs.Add(obj);
+    }
+
+    private void zdistribution()
+    {
+        //dog distribute zone
+        //distribute tester
+        //update time
+        if (Input.GetKeyDown("z"))
+        {
+            // distribute dog top side on every existed dog 
+            int tempcount = dogObjs.Count; //prevent unlimited loophole
+            for (int j = 0; j < tempcount; j++)
+            {
+                // distribute dog top side on every existed dog 
+                float templon = doglon[j];
+                float templat = doglat[j] + addLatByMeters(1000.0f);
+                //check that new lat lon is same as old one
+                Distribute_add(templat , templon , tempcount);
+                // distribute dog down side on every existed dog 
+                templon = doglon[j];
+                templat = doglat[j] + addLatByMeters(-1000.0f);
+                //check that new lat lon is same as old one
+                Distribute_add(templat , templon , tempcount);
+                // distribute dog rigth side on every existed dog 
+                templon = doglon[j] + addLonByMeters(1000.0f);
+                templat = doglat[j];
+                //check that new lat lon is same as old one
+                Distribute_add(templat , templon , tempcount);
+                // distribute dog left side on every existed dog 
+                templon = doglon[j] + addLonByMeters(-1000.0f);
+                templat = doglat[j];
+                //check that new lat lon is same as old one
+                Distribute_add(templat , templon , tempcount);
+            }
+        }
+    }
+
+    //dog distribute zone
+    private void Distribute_add(float templat , float templon , int tempcount)
+    {
+        bool check_exist = false;
+        for (int k = 0; k < tempcount; k++)
+        {
+            if (templon == doglon[k] && templat == doglat[k]) //if distribute pos got object
+            {
+                // add dog number that not exist in script yet
+                Debug.Log("Dog distribute to other dog grid,add up");
+                check_exist = true;
+            }
+        }
+        if (!check_exist) //if is new pos
+        {
+            addDogLocation(templat , templon);
+        }
     }
 }
